9adf82e13bbbcc7e8cacbd541827cb43c8e85e08
longfangsong <longfangsong@icloud.com>
2019-11-26 16:27:08 +0800

    optimize: 修改为各种设备均可解析的域名

--- src/tools/axios.ts
+++ src/tools/axios.ts
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import {jsonStringToObject} from 'json-interface2class';
 
 const instance = Axios.create({
-    baseURL: 'http://cloud.shu.xn--io0a7i:30000/',
+    baseURL: 'http://cloud.shuosc.org:30000/',
 });
 
 const dateRegex = /((\d{4})|([+-]?\d{6}))-(\d{2})-(\d{2})T\d{2}:\d{2}:\d{2}(\.\d{3})?Z/;
