bb3a7425168c02cc6b5aa9bc9cc23ce7e8cde234
longfangsong <longfangsong@icloud.com>
2019-12-20 15:44:49 +0800

    optimize: use cdn font

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css';
-import '@mdi/font/css/materialdesignicons.css';
 import Default from '@/layouts/default.vue';
 import MiddleBox from '@/layouts/middleBox.vue';
 // @ts-ignore
--- src/service-worker.js
+++ src/service-worker.js
@@ -11,7 +11,23 @@ self.addEventListener('message', async (event) => {
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 
-workbox.routing.registerRoute(/\/api\//, new workbox.strategies.NetworkFirst({
-    "cacheName": "api-cache",
-    plugins: [new workbox.expiration.Plugin({maxAgeSeconds: 86400, purgeOnQuotaError: false})]
-}), 'GET');
+workbox.routing.registerRoute(/\/api\//,
+    new workbox.strategies.NetworkFirst({
+        "cacheName": "api-cache",
+        plugins: [new workbox.expiration.Plugin({maxAgeSeconds: 86400, purgeOnQuotaError: false})]
+    }), 'GET'
+);
+
+workbox.routing.registerRoute(
+    /^https:\/\/cdn\.bootcss\.com/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'bootcss-cdn',
+    }),
+);
+
+workbox.routing.registerRoute(
+    /(^https:\/\/fonts\.font\.im)|(^https:\/\/fonts\.gstatic\.font\.im)/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'font-im-cdn',
+    }),
+);
